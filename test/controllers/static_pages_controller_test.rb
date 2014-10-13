5f52b00db2a8e5b9c7af0e100bf10d842c9ef963
Pierre Lasante <pierre.lasante@videotron.ca>
2014-10-13 06:27:45 -0700

    Started implementing the sign up page

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -18,4 +18,10 @@ class StaticPagesControllerTest < ActionController::TestCase
     assert_response :success
     assert_select "title", "About | iGrocery App"
   end
+  
+  test "should get contact" do
+    get :contact
+    assert_response :success
+    assert_select "title", "Contact | iGrocery App"
+  end
 end
