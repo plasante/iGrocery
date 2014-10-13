e192a4c4134a6d03e8ec1c73116f8b2aabee0d08
Pierre Lasante <pierre.lasante@videotron.ca>
2014-10-13 10:25:58 -0700

    The locale can now be changed by passing the /?locale=fr in a URL

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,13 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   
-  def hello
-    render text: "Hello, Pierre!"
+  before_action :set_locale
+  
+  def set_locale
+    I18n.locale = params[:locale] || I18n.default_locale
+    if not params[:locale].nil?
+      I18n.default_locale = params[:locale]
+    end
   end
+
 end
